fdf38fc502fcda84911c5f2f594a222835a43e29
nhanduc-23 <nguyennhanduc93@gmail.com>
2026-08-31 00:15:37 +0700

    cap nhat lai dem khuya

--- micromouse1/Core/Inc/main.h
+++ micromouse1/Core/Inc/main.h
@@ -1,3 +1,24 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
 #define __MAIN_H
 
@@ -8,43 +29,72 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
-/* ==================== KHAI BAO PINOUT MICROMOUSE ==================== */
+/* USER CODE BEGIN EFP */
 
-// 1. Den bao trang thai & Nut nhan dieu khien
-#define LED_PIN             GPIO_PIN_13            // LED SMD tren mach Black Pill (PC13 - Active LOW)
-#define LED_PORT            GPIOC
+/* USER CODE END EFP */
 
-#define BTN_MODE_PIN        GPIO_PIN_2             // Nut bam chuyen che do / Start (PA2 - EXTI2)
-#define BTN_MODE_PORT       GPIOA
+/* Private defines -----------------------------------------------------------*/
 
-// 2. Mach dieu khien dong co TB6612FNG
-#define MOTOR_STBY_PIN      GPIO_PIN_5             // Chan Standby kich hoat TB6612 (PA5)
-#define MOTOR_STBY_PORT     GPIOA
+/* USER CODE BEGIN Private defines */
 
-#define MOTOR_L_AIN1_PIN    GPIO_PIN_12            // Dinh huong dong co Trai AIN1 (PB12)
-#define MOTOR_L_AIN1_PORT   GPIOB
-#define MOTOR_L_AIN2_PIN    GPIO_PIN_13            // Dinh huong dong co Trai AIN2 (PB13)
-#define MOTOR_L_AIN2_PORT   GPIOB
+/* --- LED Trang thai & Nut bam chuyen che do --- */
+#define LED_PORT                 GPIOC
+#define LED_PIN                  GPIO_PIN_13  // LED Black Pill (Active LOW)
 
-#define MOTOR_R_BIN1_PIN    GPIO_PIN_14            // Dinh huong dong co Phai BIN1 (PB14)
-#define MOTOR_R_BIN1_PORT   GPIOB
-#define MOTOR_R_BIN2_PIN    GPIO_PIN_15            // Dinh huong dong co Phai BIN2 (PB15)
-#define MOTOR_R_BIN2_PORT   GPIOB
+#define BTN_MODE_PORT            GPIOA
+#define BTN_MODE_PIN             GPIO_PIN_2   // Nut nhan ngat EXTI2
 
-// 3. Chan dieu khien doi dia chi 4 cam bien Laser VL53L0X (XSHUT)
-#define XSHUT_1_PIN         GPIO_PIN_3             // Chan XSHUT Laser Trai (PA3)
-#define XSHUT_1_PORT        GPIOA
+/* --- Mach cau H TB6612 & Dieu khien huong Dong co --- */
+#define STBY_PORT                GPIOA
+#define STBY_PIN                 GPIO_PIN_5   // Chan Standby TB6612 (HIGH = On)
 
-#define XSHUT_2_PIN         GPIO_PIN_4             // Chan XSHUT Laser Truoc-Trai (PA4)
-#define XSHUT_2_PORT        GPIOA
+#define MOTOR_LEFT_IN1_PORT      GPIOB
+#define MOTOR_LEFT_IN1_PIN       GPIO_PIN_12  // Huong quay motor Trai
+#define MOTOR_LEFT_IN2_PORT      GPIOB
+#define MOTOR_LEFT_IN2_PIN       GPIO_PIN_13  // Huong quay motor Trai
 
-#define XSHUT_3_PIN         GPIO_PIN_0             // Chan XSHUT Laser Truoc-Phai (PB0)
-#define XSHUT_3_PORT        GPIOB
+#define MOTOR_RIGHT_IN1_PORT     GPIOB
+#define MOTOR_RIGHT_IN1_PIN      GPIO_PIN_14  // Huong quay motor Phai
+#define MOTOR_RIGHT_IN2_PORT     GPIOB
+#define MOTOR_RIGHT_IN2_PIN      GPIO_PIN_15  // Huong quay motor Phai
 
-#define XSHUT_4_PIN         GPIO_PIN_1             // Chan XSHUT Laser Phai (PB1)
-#define XSHUT_4_PORT        GPIOB
+/* --- Chan XSHUT Bat/Tat & Doi dia chi 4 cam bien Laser VL53L0X --- */
+#define XSHUT_LEFT_PORT          GPIOA
+#define XSHUT_LEFT_PIN           GPIO_PIN_3   // Laser Ngang Trai
+
+#define XSHUT_DIAG_L_PORT        GPIOA
+#define XSHUT_DIAG_L_PIN         GPIO_PIN_4   // Laser Cheo Trai
+
+#define XSHUT_DIAG_R_PORT        GPIOB
+#define XSHUT_DIAG_R_PIN         GPIO_PIN_0   // Laser Cheo Phai
+
+#define XSHUT_RIGHT_PORT         GPIOB
+#define XSHUT_RIGHT_PIN          GPIO_PIN_1   // Laser Ngang Phai
+
+/* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
--- micromouse1/Core/Src/main.c
+++ micromouse1/Core/Src/main.c
@@ -6,10 +6,10 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+/* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "motor.h"
 #include "encoder.h"
@@ -18,6 +18,23 @@
 // #include "pid.h"
 // #include "floodfill.h"
 /* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
 volatile uint8_t system_mode = 0; // 0: Standby, 1: Do duong, 2: Speedrun, 3: Lock
@@ -34,6 +51,14 @@ int orient = 0; // 0: FORWARD, 1: RIGHT, 2: BACKWARD, 3: LEFT
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
@@ -41,11 +66,29 @@ static void MX_GPIO_Init(void);
   */
 int main(void)
 {
-  /* MCU Configuration */
-  HAL_Init();
-  SystemClock_Config();
-  MX_GPIO_Init();
 
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
   // Khoi tao khoi dong co, Encoder va 4 cam bien Laser
   Motor_Init();        // Kich hoat PWM TIM1 dieu khien motor
@@ -96,18 +139,29 @@ int main(void)
       HAL_Delay(100);
     }
     /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
   }
+  /* USER CODE END 3 */
 }
 
-/* SystemClock_Config va MX_GPIO_Init giu nguyen */
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
+  /** Configure the main internal regulator output voltage
+  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -121,6 +175,8 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -134,21 +190,36 @@ void SystemClock_Config(void)
   }
 }
 
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
 
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -164,10 +235,33 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 }
 /* USER CODE END 4 */
 
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
+  /* USER CODE END Error_Handler_Debug */
 }
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
